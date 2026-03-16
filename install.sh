ff0d8d413e21467dd8583299c86b54d92ba87db2
yagofontanez <yagofontanez20@icloud.com>
2026-03-16 15:06:21 -0300

    feat: add install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+OS=$(uname | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m)
+
+BIN="upstat-linux"
+if [ "$OS" == "darwin" ]; then
+    BIN="upstat-mac"
+elif [ "$OS" == "linux" ]; then
+    BIN="upstat-linux"
+else
+    echo "Sistema não suportado"
+    exit 1
+fi
+
+curl -L -o /tmp/$BIN https://github.com/yagofontanez/upstat-cli/releases/latest/download/$BIN
+chmod +x /tmp/$BIN
+sudo mv /tmp/$BIN /usr/local/bin/upstat
+
+echo "UpStat CLI instalado! Rode 'upstat' para usar."
